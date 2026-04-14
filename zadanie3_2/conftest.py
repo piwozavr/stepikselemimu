8c2c798133215abb09a7a0c7236b6a3cba4575a7
Nikita <nikitak8988@gmail.com>
2026-04-14 13:49:08 +0300

    exercise 3 2

---
+++ zadanie3_2/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--language",
+        action="store",
+        default="en",
+        help="Choose language: en, fr, es, etc."
+    )
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option(
+        "prefs", {"intl.accept_languages": language}
+    )
+
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    browser.quit()
